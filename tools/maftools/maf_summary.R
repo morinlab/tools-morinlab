561128f1a5ef0d4fc89922de18fecfc3e6b5ae56
marcoalbuquerque-sfu <marcoalbuquerque.sfu@gmail.com>
2016-11-06 16:12:03 -0500

    Add maf summary plot from maftools

---
+++ tools/maftools/maf_summary.R
@@ -0,0 +1,27 @@
+require(maftools);
+library(argparse);
+
+###
+
+parser <- ArgumentParser(description="Create a Gene Lollipop using Maftools");
+
+parser$add_argument(
+    "--input_maf", "-maf",
+    required="True",
+    help="Input Variants in MAF format"
+    );
+
+parser$add_argument(
+   "--output_pdf", "-o",
+   required="True",
+   help="Output directory to store gene plots"
+   )
+
+args <- parser$parse_args();
+
+###
+
+laml = read.maf(maf = args$input_maf, removeSilent = T, useAll = F)
+pdf(args$output_pdf)
+plotmafSummary(maf = laml, rmOutlier = T, addStat = 'median', dashboard = TRUE)
+dev.off()
